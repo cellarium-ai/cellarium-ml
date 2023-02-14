393d3f95cf1396addb3056974f5a14162e0f3841
Yerdos Ordabayev <50752571+ordabayevy@users.noreply.github.com>
2023-02-14 10:54:38 -0500

    `DistributedAnnDataCollectinDataset` (#12)
    * distributed sampler draft
    
    * dataset tests
    
    * fix circular import

--- scvid/data/__init__.py
+++ scvid/data/__init__.py
@@ -1,3 +1,4 @@
+from .dadc_dataset import DistributedAnnDataCollectionDataset
 from .distributed_anndata import DistributedAnnDataCollection
 from .read import read_h5ad_file, read_h5ad_gcs, read_h5ad_local
 from .schema import AnnDataSchema
@@ -5,6 +6,7 @@ from .schema import AnnDataSchema
 __all__ = [
     "AnnDataSchema",
     "DistributedAnnDataCollection",
+    "DistributedAnnDataCollectionDataset",
     "read_h5ad_file",
     "read_h5ad_gcs",
     "read_h5ad_local",
