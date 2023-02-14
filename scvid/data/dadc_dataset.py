193eac05709654513db4ef70936acae003b721ce
Yerdos Ordabayev <yordabay@broadinstitute.org>
2023-02-14 04:55:52 +0000

    dataset tests

---
+++ scvid/data/dadc_dataset.py
@@ -0,0 +1,24 @@
+from typing import Dict
+
+from scipy.sparse import issparse
+from torch import Tensor
+from torch.utils.data import Dataset
+
+from . import DistributedAnnDataCollection
+
+
+class DistributedAnnDataCollectionDataset(Dataset):
+    def __init__(self, dadc: DistributedAnnDataCollection) -> None:
+        self.dadc = dadc
+
+    def __len__(self) -> int:
+        return len(self.dadc)
+
+    def __getitem__(self, idx: int) -> Dict[str, Tensor]:
+        """Return gene counts for a cell at idx."""
+        X = self.dadc[idx].X
+
+        data = {}
+        data["X"] = X.toarray() if issparse(X) else X
+
+        return data
