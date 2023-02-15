a064a9618a5762bbec4b79bd2be9a30ecb49da47
Yerdos Ordabayev <50752571+ordabayevy@users.noreply.github.com>
2023-02-14 22:47:19 -0500

    `PyroTrainingPlan` (#14)
    * distributed sampler draft
    
    * dataset tests
    
    * fix circular import
    
    * PyroTrainingPlan
    
    * training plan test
    
    * lint
    
    * fix docstring
    
    * fix types
    
    * address comments
    
    * fix rng

---
+++ scvid/train/training_plan.py
@@ -0,0 +1,54 @@
+from typing import Optional
+
+import pyro
+import pytorch_lightning as pl
+import torch
+
+
+class PyroTrainingPlan(pl.LightningModule):
+    """
+    Lightning module task to train Pyro scvi-tools modules.
+
+    .. note:: This is a stripped down version of the :class:`~scvi.train.LowLevelPyroTrainingPlan`.
+        https://github.com/scverse/scvi-tools/blob/bf2121975bdfc31bfb1f6feb6446c331188b47dd/scvi/train/_trainingplans.py#L745
+
+    Args:
+        pyro_module: A Pyro module. This object should have callable `model` and `guide` attributes or methods.
+        loss_fn: A Pyro loss. Should be a subclass of :class:`~pyro.infer.ELBO`.
+            If `None`, defaults to :class:`~pyro.infer.Trace_ELBO`.
+        optim: A Pytorch optimizer class, e.g., :class:`~torch.optim.Adam`. If `None`,
+            defaults to :class:`torch.optim.Adam`.
+        optim_kwargs: Keyword arguments for optimiser. If `None`, defaults to `dict(lr=1e-3)`.
+    """
+
+    def __init__(
+        self,
+        pyro_module: pyro.nn.PyroModule,
+        loss_fn: Optional[pyro.infer.ELBO] = None,
+        optim: Optional[torch.optim.Optimizer] = None,
+        optim_kwargs: Optional[dict] = None,
+    ):
+        super().__init__()
+        self.module = pyro_module
+
+        optim_kwargs = optim_kwargs if isinstance(optim_kwargs, dict) else dict()
+        if "lr" not in optim_kwargs.keys():
+            optim_kwargs.update({"lr": 1e-3})
+        self.optim_kwargs = optim_kwargs
+
+        self.loss_fn = (
+            pyro.infer.Trace_ELBO().differentiable_loss if loss_fn is None else loss_fn
+        )
+        self.optim = torch.optim.Adam if optim is None else optim
+
+    def training_step(self, batch, batch_idx):
+        """Training step for Pyro training."""
+        args, kwargs = self.module._get_fn_args_from_batch(batch)
+        loss = self.loss_fn(self.module.model, self.module.guide, *args, **kwargs)
+        # Logging to TensorBoard by default
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        """Configure optimizers for the model."""
+        return self.optim(self.module.parameters(), **self.optim_kwargs)
