aad2aab56d68d39ec33489dc3e9439c2632624fd
Yerdos Ordabayev <50752571+ordabayevy@users.noreply.github.com>
2023-02-23 10:48:45 -0500

    `OnePassMeanVarStd` (#18)
    * OnePassMeanStd
    
    * fixes
    
    * lint
    
    * rename test

--- scvid/module/__init__.py
+++ scvid/module/__init__.py
@@ -1,3 +1,4 @@
 from ._probabilisticpca import ProbabilisticPCAPyroModule
+from .onepass_mean_var_std import OnePassMeanVarStd
 
-__all__ = ["ProbabilisticPCAPyroModule"]
+__all__ = ["ProbabilisticPCAPyroModule", "OnePassMeanVarStd"]
