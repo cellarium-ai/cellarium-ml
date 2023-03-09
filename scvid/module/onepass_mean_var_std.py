6625764c8b3ea060289f24361cf989c043d27bd6
Yerdos Ordabayev <yordabay@broadinstitute.org>
2023-03-09 02:40:46 +0000

    revert changes

--- scvid/module/onepass_mean_var_std.py
+++ scvid/module/onepass_mean_var_std.py
@@ -3,25 +3,6 @@ from typing import Dict, Iterable, Optional, Tuple
 import torch
 import torch.nn as nn
 
-import torch.distributed as dist
-
-
-class GatherLayer(torch.autograd.Function):
-    """Gather tensors from all process, supporting backward propagation."""
-
-    @staticmethod
-    def forward(ctx, input):
-        ctx.save_for_backward(input)
-        output = [torch.zeros_like(input) for _ in range(dist.get_world_size())]
-        dist.all_gather(output, input)
-        return tuple(output)
-
-    @staticmethod
-    def backward(ctx, *grads):
-        (input,) = ctx.saved_tensors
-        grad_out = torch.zeros_like(input)
-        grad_out[:] = grads[dist.get_rank()]
-        return grad_out
 
 class OnePassMeanVarStd(nn.Module):
     """
@@ -46,14 +27,9 @@ class OnePassMeanVarStd(nn.Module):
     def forward(self, x_ng: torch.Tensor) -> None:
         if self.transform is not None:
             x_ng = self.transform(x_ng)
-        # print("RANK: ", dist.get_rank())
-        # print("X_NG SHAPE: ", x_ng.shape)
-        x_ng = torch.cat(GatherLayer.apply(x_ng), dim=0)
-        # print("GATHERED X_NG SHAPE: ", x_ng.shape)
         self.x_sums += x_ng.sum(dim=0)
         self.x_squared_sums += (x_ng**2).sum(dim=0)
         self.x_size += x_ng.shape[0]
-        # print("X SIZE: ", self.x_size)
 
     @property
     def mean(self) -> torch.Tensor:
