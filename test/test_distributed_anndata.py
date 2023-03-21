7cf04347a90a1fd153be75e0811979177d765479
Yerdos Ordabayev <yordabay@broadinstitute.org>
2023-03-21 15:09:32 +0000

    lint

--- test/test_distributed_anndata.py
+++ test/test_distributed_anndata.py
@@ -103,9 +103,7 @@ def test_init_dat(dat):
 @pytest.mark.parametrize("last_shard_size", [1, 2, 3, None])
 def test_init_shard_size(adatas_path, num_shards, last_shard_size):
     shard_size = 2
-    filenames = str(os.path.join(adatas_path, f"adata.{{000..{num_shards-1:03}}}.h5ad"))
     dadc = DistributedAnnDataCollection(
-        # filenames,
         "gs://dsp-cellarium-cas-public/test-data/benchmark_v1.{000..001}.h5ad",
         shard_size=shard_size,
         last_shard_size=last_shard_size,
