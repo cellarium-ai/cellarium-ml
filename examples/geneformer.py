ab0a7a48fc9948c0d1bdfd2f228db193ed95ce71
Yerdos Ordabayev <yordabay@broadinstitute.org>
2023-08-28 19:54:17 +0000

    fix shard size in examples

--- examples/geneformer.py
+++ examples/geneformer.py
@@ -12,7 +12,7 @@ Example run::
     python examples/geneformer.py fit \
         --model.module cellarium.ml.module.GeneformerFromCLI \
         --data.filenames "gs://dsp-cellarium-cas-public/test-data/benchmark_v1.{000..003}.h5ad" \
-        --data.shard_size 10_000 --data.max_cache_size 2 --data.batch_size 5 \
+        --data.shard_size 100 --data.max_cache_size 2 --data.batch_size 5 \
         --data.num_workers 1 \
         --trainer.accelerator gpu --trainer.devices 1 --trainer.default_root_dir runs/geneformer \
         --trainer.max_steps 10
