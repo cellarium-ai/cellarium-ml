ad13e01fb6c922ed5cf81144d089a41856fe8b16
Yerdos Ordabayev <yordabay@broadinstitute.org>
2023-09-07 21:25:40 +0000

    fix test

--- test/test_examples.py
+++ test/test_examples.py
@@ -7,6 +7,8 @@ from subprocess import check_call
 
 import pytest
 
+from .common import requires_crick
+
 TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 EXAMPLES_DIR = os.path.join(os.path.dirname(TESTS_DIR), "examples")
 
@@ -50,14 +52,15 @@ EXAMPLES = [
         "--trainer.accelerator cpu --trainer.devices 1 "
         "--trainer.callbacks cellarium.ml.callbacks.PredictionWriter --trainer.callbacks.output_dir ./output"
     ),
-    (
+    pytest.param(
         "tdigest.py fit "
         "--model.module.class_path cellarium.ml.module.TDigestFromCLI "
         "--data.filenames "
         "https://storage.googleapis.com/dsp-cellarium-cas-public/test-data/benchmark_v1.{000..001}.h5ad "
         "--data.shard_size 100 --data.max_cache_size 2 --data.batch_size 50 "
         "--data.num_workers 2 "
-        "--trainer.accelerator cpu --trainer.devices 1"
+        "--trainer.accelerator cpu --trainer.devices 1",
+        marks=requires_crick,
     ),
 ]
 
