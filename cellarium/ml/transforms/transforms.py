97887f04a4835aa6b21d04e0eb2cbf47cc32d17d
Yerdos Ordabayev <yordabay@broadinstitute.org>
2023-10-13 21:07:14 +0000

    misc

--- cellarium/ml/transforms/transforms.py
+++ cellarium/ml/transforms/transforms.py
@@ -8,55 +8,6 @@ import torch
 from torch import nn
 
 
-class NormalizeTotal(nn.Module):
-    """
-    Normalize total gene counts per cell to target count.
-
-    .. math::
-
-        \\mathrm{total\\_mrna\\_umis}_n = \\sum_{g=1}^G x_{ng}
-
-        y_{ng} = \\frac{\\mathrm{target\\_count} \\times x_{ng}}{\\mathrm{total\\_mrna\\_umis}_n + \\mathrm{eps}}
-
-    Args:
-        target_count:
-            Target gene epxression count.
-        eps:
-            A value added to the denominator for numerical stability.
-    """
-
-    def __init__(
-        self,
-        target_count: int = 10_000,
-        eps: float = 1e-6,
-    ) -> None:
-        super().__init__()
-        self.target_count = target_count
-        self.eps = eps
-
-    def forward(
-        self,
-        x_ng: torch.Tensor,
-        total_mrna_umis_n: torch.Tensor | None = None,
-    ) -> torch.Tensor:
-        """
-        Args:
-            x_ng:
-                Gene counts.
-            total_mrna_umis_n:
-                Total mRNA UMI counts per cell. If ``None``, it is computed from ``x_ng``.
-
-        Returns:
-            Gene counts normalized to target count.
-        """
-        if total_mrna_umis_n is None:
-            total_mrna_umis_n = x_ng.sum(dim=-1)
-        return self.target_count * x_ng / (total_mrna_umis_n[:, None] + self.eps)
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(target_count={self.target_count}, eps={self.eps})"
-
-
 class DivideByScale(nn.Module):
     """
     Divide gene counts by a scale.
@@ -108,6 +59,53 @@ class DivideByScale(nn.Module):
         )
 
 
+class Filter(nn.Module):
+    """
+    Filter gene counts by a list of features.
+
+    .. math::
+
+        \\mathrm{mask}_g = \\mathrm{feature}_g \\in \\mathrm{filter\\_list}
+
+        y_{ng} = x_{ng}[:, \\mathrm{mask}_g]
+
+    Args:
+        filter_list: A list of features to filter by.
+    """
+
+    def __init__(self, filter_list: np.ndarray) -> None:
+        super().__init__()
+        self.filter_list = filter_list
+
+    @cache
+    def filter(self, feature_list: np.ndarray) -> np.ndarray[bool]:
+        """
+        Args:
+            feature_list: The list of the variable names in the input data.
+
+        Returns:
+            A boolean mask of the features to filter by.
+        """
+        return np.isin(feature_list, self.filter_list)
+
+    def forward(self, x_ng: torch.Tensor, feature_list: np.ndarray) -> torch.Tensor:
+        """
+        Args:
+            x_ng: Gene counts.
+            feature_list: The list of the variable names in the input data.
+
+        Returns:
+            Filtered gene counts.
+        """
+        assert x_ng.shape[1] == len(feature_list), "The number of x_ng columns must match the feature_list length."
+
+        mask = self.filter(feature_list)
+        return x_ng[:, mask]
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(filter_list={self.filter_list})"
+
+
 class Log1p(nn.Module):
     """
     Log1p transform gene counts.
@@ -132,6 +130,55 @@ class Log1p(nn.Module):
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}()"
+
+
+class NormalizeTotal(nn.Module):
+    """
+    Normalize total gene counts per cell to target count.
+
+    .. math::
+
+        \\mathrm{total\\_mrna\\_umis}_n = \\sum_{g=1}^G x_{ng}
+
+        y_{ng} = \\frac{\\mathrm{target\\_count} \\times x_{ng}}{\\mathrm{total\\_mrna\\_umis}_n + \\mathrm{eps}}
+
+    Args:
+        target_count:
+            Target gene epxression count.
+        eps:
+            A value added to the denominator for numerical stability.
+    """
+
+    def __init__(
+        self,
+        target_count: int = 10_000,
+        eps: float = 1e-6,
+    ) -> None:
+        super().__init__()
+        self.target_count = target_count
+        self.eps = eps
+
+    def forward(
+        self,
+        x_ng: torch.Tensor,
+        total_mrna_umis_n: torch.Tensor | None = None,
+    ) -> torch.Tensor:
+        """
+        Args:
+            x_ng:
+                Gene counts.
+            total_mrna_umis_n:
+                Total mRNA UMI counts per cell. If ``None``, it is computed from ``x_ng``.
+
+        Returns:
+            Gene counts normalized to target count.
+        """
+        if total_mrna_umis_n is None:
+            total_mrna_umis_n = x_ng.sum(dim=-1)
+        return self.target_count * x_ng / (total_mrna_umis_n[:, None] + self.eps)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(target_count={self.target_count}, eps={self.eps})"
 
 
 class ZScore(nn.Module):
@@ -192,47 +239,3 @@ class ZScore(nn.Module):
             f"{self.__class__.__name__}(mean_g={self.mean_g}, std_g={self.std_g}, eps={self.eps}, "
             f"feature_schema={self.feature_schema})"
         )
-
-
-class Filter(nn.Module):
-    """
-    Filter gene counts by a list of features.
-
-    .. math::
-
-        \\mathrm{mask}_g = \\mathrm{feature}_g \\in \\mathrm{filter\\_list}
-
-        y_{ng} = x_{ng}[:, \\mathrm{mask}_g]
-
-    Args:
-        filter_list: A list of features to filter by.
-    """
-
-    def __init__(self, filter_list: np.ndarray) -> None:
-        super().__init__()
-        self.filter_list = filter_list
-
-    @cache
-    def filter(self, feature_list: np.ndarray) -> np.ndarray[bool]:
-        """
-        Args:
-            feature_list: The list of the variable names in the input data.
-
-        Returns:
-            A boolean mask of the features to filter by.
-        """
-        return np.isin(feature_list, self.filter_list)
-
-    def forward(self, x_ng: torch.Tensor, feature_list: np.ndarray) -> torch.Tensor:
-        """
-        Args:
-            x_ng: Gene counts.
-            feature_list: The list of the variable names in the input data.
-
-        Returns:
-            Filtered gene counts.
-        """
-        assert x_ng.shape[1] == len(feature_list), "The number of x_ng columns must match the feature_list length."
-
-        mask = self.filter(feature_list)
-        return x_ng[:, mask]
