d0588937442b7279319585368c31249d85e88af9
Yerdos Ordabayev <yordabay@broadinstitute.org>
2023-10-13 22:12:43 +0000

    fix test

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -54,7 +54,7 @@ def test_log_normalize_mean_std(x_ng: torch.Tensor, log_normalize: torch.nn.Sequ
 )
 def test_filter(x_ng: torch.Tensor, filter_list: list[str]):
     transform = Filter(np.array(filter_list))
-    feature_list = [f"gene_{i}" for i in range(g)]
+    feature_list = np.array([f"gene_{i}" for i in range(g)])
     new_x_ng = transform(x_ng, feature_list)
     assert new_x_ng.shape[1] == len(filter_list)
     assert new_x_ng.shape[0] == x_ng.shape[0]
