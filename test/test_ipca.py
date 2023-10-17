12471dea50c76547d3c9e9229ca77daa2e85413f
Yerdos Ordabayev <yordabay@broadinstitute.org>
2023-10-17 16:46:15 -0400

    Override `load_from_checkpoint` classmethod of `TrainingPlan` (#91)
    * wip
    
    * classmethod load_from_checkpoint
    
    * fixes
    
    * fix tests
    
    * fix mypy
    
    * fix tests
    
    * fix tests
    
    * save config
    
    * fix tests
    
    * rm ModuleCheckpoint callback
    
    * add docstring
    
    * multi_device tests
    
    * fix multi_device tests
    
    * rm log_every_n_steps
    
    * address comments

--- test/test_ipca.py
+++ test/test_ipca.py
@@ -1,6 +1,7 @@
 # Copyright Contributors to the Cellarium project.
 # SPDX-License-Identifier: BSD-3-Clause
 
+import math
 import os
 from pathlib import Path
 
@@ -10,18 +11,17 @@ import pytest
 import torch
 from lightning.pytorch.strategies import DDPStrategy
 
-from cellarium.ml.callbacks import ModuleCheckpoint
+from cellarium.ml.data.util import collate_fn
 from cellarium.ml.module import IncrementalPCA, IncrementalPCAFromCLI
 from cellarium.ml.train import TrainingPlan
 from cellarium.ml.transforms import ZScoreLog1pNormalize
 
 from .common import TestDataset
 
-n, g = 10000, 100
-
 
 @pytest.fixture
 def x_ng():
+    n, g = 10000, 100
     rng = torch.Generator()
     rng.manual_seed(1465)
     mean_g = torch.randn((g,), generator=rng)
@@ -32,17 +32,18 @@ def x_ng():
 @pytest.mark.parametrize("perform_mean_correction", [False, True])
 @pytest.mark.parametrize("batch_size", [10_000, 5000, 1000, 500, 250])
 @pytest.mark.parametrize("k", [30, 50, 80])
-def test_incremental_pca_multi_device(x_ng: np.ndarray, perform_mean_correction: bool, batch_size: int, k: int):
+def test_incremental_pca_multi_device(x_ng: torch.Tensor, perform_mean_correction: bool, batch_size: int, k: int):
     n, g = x_ng.shape
-    x_ng_centered = x_ng - x_ng.mean(axis=0)
+    x_ng_centered = x_ng - x_ng.mean(dim=0)
     devices = int(os.environ.get("TEST_DEVICES", "1"))
     batch_size = batch_size // devices
 
     # dataloader
     train_loader = torch.utils.data.DataLoader(
-        TestDataset(x_ng if perform_mean_correction else x_ng_centered),
+        TestDataset((x_ng if perform_mean_correction else x_ng_centered).numpy()),
         batch_size=batch_size,
         shuffle=False,
+        collate_fn=collate_fn,
     )
     # model
     ipca = IncrementalPCA(
@@ -79,51 +80,51 @@ def test_incremental_pca_multi_device(x_ng: np.ndarray, perform_mean_correction:
     assert ipca.x_size == n
     np.testing.assert_allclose(
         ipca.x_mean_g,
-        (x_ng if perform_mean_correction else x_ng_centered).mean(axis=0),
+        (x_ng if perform_mean_correction else x_ng_centered).mean(dim=0),
         atol=1e-5,
     )
 
 
-@pytest.mark.parametrize(
-    "checkpoint_kwargs",
-    [
-        {
-            "save_on_train_end": True,
-            "save_on_train_epoch_end": False,
-            "save_on_train_batch_end": False,
-        },
-        {
-            "save_on_train_end": False,
-            "save_on_train_epoch_end": True,
-            "save_on_train_batch_end": False,
-        },
-        {
-            "save_on_train_end": False,
-            "save_on_train_epoch_end": False,
-            "save_on_train_batch_end": True,
-        },
-    ],
-)
-def test_module_checkpoint(tmp_path: Path, checkpoint_kwargs: dict):
+def test_load_from_checkpoint_multi_device(tmp_path: Path):
+    n, g = 3, 2
+    devices = int(os.environ.get("TEST_DEVICES", "1"))
     # dataloader
-    train_loader = torch.utils.data.DataLoader(TestDataset(np.arange(6).reshape(3, 2)))
+    train_loader = torch.utils.data.DataLoader(
+        TestDataset(np.arange(n * g).reshape(n, g)),
+        collate_fn=collate_fn,
+    )
     # model
-    model = IncrementalPCAFromCLI(2, 1, perform_mean_correction=True, target_count=10)
-    training_plan = TrainingPlan(model)
+    init_args = {"g_genes": g, "k_components": 1, "perform_mean_correction": True, "target_count": 10}
+    model = IncrementalPCAFromCLI(**init_args)  # type: ignore[arg-type]
+    config = {
+        "model": {
+            "module": {
+                "class_path": "cellarium.ml.module.IncrementalPCAFromCLI",
+                "init_args": init_args,
+            }
+        }
+    }
+    training_plan = TrainingPlan(model, config=config)
     # trainer
-    checkpoint_kwargs["dirpath"] = tmp_path
-    module_checkpoint = ModuleCheckpoint(**checkpoint_kwargs)
+    strategy = DDPStrategy(broadcast_buffers=False) if devices > 1 else "auto"
     trainer = pl.Trainer(
-        max_epochs=1,
         accelerator="cpu",
-        callbacks=[module_checkpoint],
-        log_every_n_steps=1,
+        strategy=strategy,  # type: ignore[arg-type]
+        devices=devices,
+        max_epochs=1,
+        default_root_dir=tmp_path,
     )
     # fit
     trainer.fit(training_plan, train_dataloaders=train_loader)
+
+    # run tests only for rank 0
+    if trainer.global_rank != 0:
+        return
+
     # load model from checkpoint
-    assert os.path.exists(os.path.join(tmp_path, "module_checkpoint.pt"))
-    loaded_model: IncrementalPCAFromCLI = torch.load(os.path.join(tmp_path, "module_checkpoint.pt"))
+    ckpt_path = tmp_path / f"lightning_logs/version_0/checkpoints/epoch=0-step={math.ceil(n / devices)}.ckpt"
+    assert ckpt_path.is_file()
+    loaded_model: IncrementalPCAFromCLI = TrainingPlan.load_from_checkpoint(ckpt_path).module
     # assert
     assert isinstance(model.transform, ZScoreLog1pNormalize)
     assert isinstance(loaded_model.transform, ZScoreLog1pNormalize)
