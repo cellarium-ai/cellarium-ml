3acf8d417dbf8c29a55b3dd74e7ea9e59cd7dabd
Yerdos Ordabayev <yordabay@broadinstitute.org>
2023-10-19 17:28:28 +0000

    address comments

--- cellarium/ml/cli.py
+++ cellarium/ml/cli.py
@@ -333,12 +333,19 @@ def main(args: ArgsType = None) -> None:
             or the ``model_name`` key if ``args`` is a dictionary or ``Namespace``.
     """
     if isinstance(args, (dict, Namespace)):
+        assert "model_name" in args, "'model_name' key must be specified in args"
         model_name = args.pop("model_name")
     elif isinstance(args, list):
+        assert len(args) > 0, "'model_name' must be specified as the first argument in args"
         model_name = args.pop(0)
     elif args is None:
         args = sys.argv[1:].copy()
+        assert len(args) > 0, "'model_name' must be specified after cellarium-ml"
         model_name = args.pop(0)
+
+    assert (
+        model_name in REGISTERED_MODELS
+    ), f"'model_name' must be one of {list(REGISTERED_MODELS.keys())}. Got '{model_name}'"
     model_cli = REGISTERED_MODELS[model_name]
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message="LightningCLI's args parameter is intended to run from within Python")
