67a43baa6384a8063d4263bec6a5d48deed93201
Yerdos Ordabayev <yordabay@broadinstitute.org>
2024-01-11 15:51:18 +0000

    fix the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,6 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
-    "sphinx_autodoc_typehints",
     "sphinx_copybutton",
     "sphinx_rtd_theme",
 ]
@@ -32,13 +31,12 @@ exclude_patterns = []
 
 autodoc_inherit_docstrings = False
 autodoc_member_order = "bysource"
-
-# Add a default annotation
-
-typehints_defaults = "comma"
+autodoc_typehints = "both"
+autodoc_typehints_format = "short"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+html_css_files = ["custom.css"]
