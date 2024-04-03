a00cc716982f0a5c520ee89ea8d76b6260b262b3
Stephen Fleming <sjf11@case.edu>
2024-04-03 16:38:33 +0000

    Clean up docstring

--- cellarium/ml/models/scvi.py
+++ cellarium/ml/models/scvi.py
@@ -252,98 +252,65 @@ class SingleCellVariationalInference(CellariumModel, PredictMixin):
        <https://www.nature.com/articles/s41592-018-0229-2>`_.
 
     Args:
-        var_names_g:
-            The variable names schema for the input data validation.
-        encoder_hidden:
-            Number of hidden units in all hidden layers of the encoder and decoder.
-        decoder_hidden:
-            Number of hidden units in all hidden layers of the decoder.
-        n_latent:
-            Dimension of the latent space.
-        n_batch:
-            Number of total batches in the dataset.
-        n_layers:
-            Number of hidden layers in the encoder and decoder.
-        n_continuous_cov:
-            Number of continuous covariates.
-        n_cats_per_cov:
-            A list of integers containing the number of categories for each categorical covariate.
-        dropout_rate:
-            Dropout rate for hidden units in the encoder only.
-        dispersion:
-            Flexibility of the dispersion parameter when ``gene_likelihood`` is either ``"nb"`` or
+        var_names_g: The variable names schema for the input data validation.
+        encoder: Dict specifying the encoder configuration.
+        decoder: Dict specifying the decoder configuration.
+        n_latent: Dimension of the latent space.
+        n_batch: Number of total batches in the dataset.
+        batch_bais_sampled: True to sample the batch-specific biases from their distributions.
+        n_continuous_cov: Number of continuous covariates.
+        n_cats_per_cov: A list of integers containing the number of categories for each categorical covariate.
+        dropout_rate: Dropout rate for hidden units in the encoder and decoder.
+        dispersion: Flexibility of the dispersion parameter when ``gene_likelihood`` is either ``"nb"`` or
             ``"zinb"``. One of the following:
                 * ``"gene"``: parameter is constant per gene across cells.
                 * ``"gene-batch"``: parameter is constant per gene per batch.
                 * ``"gene-label"``: parameter is constant per gene per label.
                 * ``"gene-cell"``: parameter is constant per gene per cell.
-        log_variational:
-            If ``True``, use :func:`~torch.log1p` on input data before encoding for numerical stability
+        log_variational: If ``True``, use :func:`~torch.log1p` on input data before encoding for numerical stability
             (not normalization).
-        gene_likelihood:
-            Distribution to use for reconstruction in the generative process. One of the following:
+        gene_likelihood: Distribution to use for reconstruction in the generative process. One of the following:
                 * ``"nb"``: :class:`~scvi.distributions.NegativeBinomial`.
                 * ``"zinb"``: :class:`~scvi.distributions.ZeroInflatedNegativeBinomial`.
                 * ``"poisson"``: :class:`~scvi.distributions.Poisson`.
-        latent_distribution:
-            Distribution to use for the latent space. One of the following:
+        latent_distribution: Distribution to use for the latent space. One of the following:
                 * ``"normal"``: isotropic normal.
                 * ``"ln"``: logistic normal with normal params N(0, 1).
-        encode_covariates:
-            If ``True``, covariates are concatenated to gene expression prior to passing through
+        encode_covariates: If ``True``, covariates are concatenated to gene expression prior to passing through
             the encoder(s). Else, only gene expression is used.
-        deeply_inject_covariates:
-            If ``True`` and ``n_layers > 1``, covariates are concatenated to the outputs of hidden
-            layers in the encoder(s) (if ``encoder_covariates`` is ``True``) and the decoder prior to
-            passing through the next layer.
-        batch_representation:
-            ``EXPERIMENTAL`` Method for encoding batch information. One of the following:
+        batch_representation: ``EXPERIMENTAL`` Method for encoding batch information. One of the following:
                 * ``"one-hot"``: represent batches with one-hot encodings.
                 * ``"embedding"``: represent batches with continuously-valued embeddings using
                 :class:`~scvi.nn.Embedding`.
             Note that batch representations are only passed into the encoder(s) if
             ``encode_covariates`` is ``True``.
-        use_batch_norm:
-            Specifies where to use :class:`~torch.nn.BatchNorm1d` in the model. One of the following:
+        use_batch_norm: Specifies where to use :class:`~torch.nn.BatchNorm1d` in the model. One of the following:
                 * ``"none"``: don't use batch norm in either encoder(s) or decoder.
                 * ``"encoder"``: use batch norm only in the encoder(s).
                 * ``"decoder"``: use batch norm only in the decoder.
                 * ``"both"``: use batch norm in both encoder(s) and decoder.
             Note: if ``use_layer_norm`` is also specified, both will be applied (first
             :class:`~torch.nn.BatchNorm1d`, then :class:`~torch.nn.LayerNorm`).
-        use_layer_norm:
-            Specifies where to use :class:`~torch.nn.LayerNorm` in the model. One of the following:
+        use_layer_norm: Specifies where to use :class:`~torch.nn.LayerNorm` in the model. One of the following:
                 * ``"none"``: don't use layer norm in either encoder(s) or decoder.
                 * ``"encoder"``: use layer norm only in the encoder(s).
                 * ``"decoder"``: use layer norm only in the decoder.
                 * ``"both"``: use layer norm in both encoder(s) and decoder.
             Note: if ``use_batch_norm`` is also specified, both will be applied (first
             :class:`~torch.nn.BatchNorm1d`, then :class:`~torch.nn.LayerNorm`).
-        use_size_factor_key:
-            If ``True``, use the :attr:`~anndata.AnnData.obs` column as defined by the
+        use_size_factor_key: If ``True``, use the :attr:`~anndata.AnnData.obs` column as defined by the
             ``size_factor_key`` parameter in the model's ``setup_anndata`` method as the scaling
             factor in the mean of the conditional distribution. Takes priority over
             ``use_observed_lib_size``.
-        use_observed_lib_size:
-            If ``True``, use the observed library size for RNA as the scaling factor in the mean of the
+        use_observed_lib_size: If ``True``, use the observed library size for RNA as the scaling factor in the mean of the
             conditional distribution.
-        library_log_means:
-            :class:`~numpy.ndarray` of shape ``(1, n_batch)`` of means of the log library sizes that
+        library_log_means: :class:`~numpy.ndarray` of shape ``(1, n_batch)`` of means of the log library sizes that
             parameterize the prior on library size if ``use_size_factor_key`` is ``False`` and
             ``use_observed_lib_size`` is ``False``.
-        library_log_vars:
-            :class:`~numpy.ndarray` of shape ``(1, n_batch)`` of variances of the log library sizes
+        library_log_vars: :class:`~numpy.ndarray` of shape ``(1, n_batch)`` of variances of the log library sizes
             that parameterize the prior on library size if ``use_size_factor_key`` is ``False`` and
             ``use_observed_lib_size`` is ``False``.
-        var_activation:
-            Callable used to ensure positivity of the variance of the variational distribution. Passed
-            into :class:`~scvi.nn.Encoder`. Defaults to :func:`~torch.exp`.
-        extra_encoder_kwargs:
-            Additional keyword arguments passed into :class:`~scvi.nn.Encoder`.
-        extra_decoder_kwargs:
-            Additional keyword arguments passed into :class:`~scvi.nn.DecoderSCVI`.
-        batch_embedding_kwargs:
-            Keyword arguments passed into :class:`~scvi.nn.Embedding` if ``batch_representation`` is
+        batch_embedding_kwargs: Keyword arguments passed into :class:`~scvi.nn.Embedding` if ``batch_representation`` is
             set to ``"embedding"``.
     """
 
@@ -370,9 +337,6 @@ class SingleCellVariationalInference(CellariumModel, PredictMixin):
         use_observed_lib_size: bool = True,
         library_log_means: np.ndarray | None = None,
         library_log_vars: np.ndarray | None = None,
-        # var_activation: Callable[[torch.Tensor], torch.Tensor] = None,
-        # extra_encoder_kwargs: dict | None = None,
-        # extra_decoder_kwargs: dict | None = None,
         batch_embedding_kwargs: dict | None = None,
     ):
         super().__init__()
@@ -387,6 +351,9 @@ class SingleCellVariationalInference(CellariumModel, PredictMixin):
         self.encode_covariates = encode_covariates
         self.use_size_factor_key = use_size_factor_key
         self.use_observed_lib_size = use_size_factor_key or use_observed_lib_size
+
+        if batch_bais_sampled:
+            raise NotImplementedError("batch_bais_sampled is not yet implemented in SingleCellVariationalInference")
 
         if not self.use_observed_lib_size:
             if library_log_means is None or library_log_vars is None:
