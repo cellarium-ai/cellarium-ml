27982fa3af2df6908063180fa6acd6aae5a8647b
Brice Wang <brice.l.wang@gmail.com>
2024-04-23 15:10:19 +0000

    Implement contrastive learning model
    WIP model init bug
    
    Rm pdb trace

--- cellarium/ml/models/__init__.py
+++ cellarium/ml/models/__init__.py
@@ -1,22 +1,26 @@
 # Copyright Contributors to the Cellarium project.
 # SPDX-License-Identifier: BSD-3-Clause
 
+from cellarium.ml.models.contrastive_mlp import ContrastiveMLP
 from cellarium.ml.models.geneformer import Geneformer
 from cellarium.ml.models.incremental_pca import IncrementalPCA
 from cellarium.ml.models.logistic_regression import LogisticRegression
 from cellarium.ml.models.model import CellariumModel, PredictMixin, ValidateMixin
 from cellarium.ml.models.mu_linear import MuLinear, abcdParameter
+from cellarium.ml.models.nt_xent import NT_Xent
 from cellarium.ml.models.onepass_mean_var_std import OnePassMeanVarStd
 from cellarium.ml.models.probabilistic_pca import ProbabilisticPCA
 from cellarium.ml.models.tdigest import TDigest
 
 __all__ = [
     "CellariumModel",
+    "ContrastiveMLP",
     "Geneformer",
     "IncrementalPCA",
     "LogisticRegression",
     "abcdParameter",
     "MuLinear",
+    "NT_Xent",
     "OnePassMeanVarStd",
     "PredictMixin",
     "ProbabilisticPCA",
---
+++ cellarium/ml/transforms/binomial_resample.py
@@ -0,0 +1,50 @@
+# Copyright Contributors to the Cellarium project.
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+import torch
+from torch import nn
+from torch.distributions import Bernoulli, Binomial, Uniform
+
+
+class BinomialResample(nn.Module):
+    """
+    Binomial resampling of gene counts.
+
+    For each count, the parameter to the binomial distribution
+    is independently and uniformly sampled according to the
+    bounding parameters, yielding the parameter matrix p_ng.
+
+    .. math::
+
+        y_{ng} = Binomial(n=x_{ng}, p=p_{ng})
+
+    Args:
+        p_binom_min:
+            Lower bound on binomial distribution parameter.
+        p_binom_max:
+            Upper bound on binomial distribution parameter.
+    """
+
+    def __init__(self, p_binom_min, p_binom_max, p_apply):
+        super().__init__()
+
+        self.p_binom_min = p_binom_min
+        self.p_binom_max = p_binom_max
+        self.p_apply = p_apply
+
+    def forward(self, x_ng: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x_ng: Gene counts.
+
+        Returns:
+            Binomially resampled gene counts.
+        """
+        p_binom_ng = Uniform(self.p_binom_min, self.p_binom_max).sample(x_ng.shape).type_as(x_ng)
+        p_apply_n = Bernoulli(probs=self.p_apply).sample(x_ng.shape[:1]).type_as(x_ng).bool()
+
+        x_aug = Binomial(total_count=x_ng, probs=p_binom_ng).sample()
+        
+        x_ng = torch.where(p_apply_n.unsqueeze(1), x_ng, x_aug)
+        return {'x_ng': x_ng}
