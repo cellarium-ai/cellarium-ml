46f54dc19d9d9c9ecd3ce299a53467223c3a53d8
Yerdos Ordabayev <yordabay@broadinstitute.org>
2024-04-24 21:52:12 +0000

    CellariumGPT with CE loss

--- cellarium/ml/models/cellarium_dit.py
+++ cellarium/ml/models/cellarium_dit.py
@@ -23,64 +23,6 @@ from cellarium.ml.utilities.testing import (
     assert_columns_and_array_lengths_equal,
 )
 
-
-class NegativeBinomial(dist.Distribution):
-    """Negative binomial distribution.
-
-    Args:
-        mu:
-            Mean of the distribution.
-        theta:
-            Inverse dispersion.
-    """
-
-    arg_constraints = {"mu": constraints.greater_than_eq(0), "theta": constraints.greater_than_eq(0)}
-    support = constraints.nonnegative_integer
-
-    def __init__(self, mu: torch.Tensor, theta: torch.Tensor, eps: float = 1e-8, validate_args: bool = False) -> None:
-        self.mu, self.theta = broadcast_all(mu, theta)
-        if isinstance(mu, Number) and isinstance(theta, Number):
-            batch_shape = torch.Size()
-        else:
-            batch_shape = self.mu.size()
-        self.eps = eps
-        super().__init__(batch_shape, validate_args=validate_args)
-
-    @property
-    def mean(self) -> torch.Tensor:
-        return self.mu
-
-    @property
-    def variance(self) -> torch.Tensor:
-        return self.mu + (self.mu**2) / self.theta
-
-    def log_prob(self, value: torch.Tensor) -> torch.Tensor:
-        if self._validate_args:
-            self._validate_sample(value)
-
-        # Original implementation from scVI:
-        #
-        #   log_theta_mu_eps = torch.log(self.theta + self.mu + self.eps)
-        #   return (
-        #       self.theta * (torch.log(self.theta + self.eps) - log_theta_mu_eps)
-        #       + value * (torch.log(self.mu + self.eps) - log_theta_mu_eps)
-        #       + torch.lgamma(value + self.theta)
-        #       - torch.lgamma(self.theta)
-        #       - torch.lgamma(value + 1)
-        #   )
-        delta = torch.where(
-            (value / self.theta < 1e-2) & (self.theta > 1e2),
-            (value + self.theta - 0.5) * torch.log1p(value / self.theta) - value,
-            (value + self.theta).lgamma() - self.theta.lgamma() - torch.xlogy(value, self.theta),
-        )
-        return (
-            delta
-            - (value + self.theta) * torch.log1p(self.mu / self.theta)
-            - (value + 1).lgamma()
-            + torch.xlogy(value, self.mu)
-        )
-
-
 backend_map = {
     "math": {"enable_math": True, "enable_flash": False, "enable_mem_efficient": False},
     "flash": {"enable_math": False, "enable_flash": True, "enable_mem_efficient": False},
