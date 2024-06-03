38804ddf123dcf31dc89cfe6d0aaba6f60b4fc03
Yang Xu <35667575+ImXman@users.noreply.github.com>
2024-06-03 14:09:11 -0400

    update nmf.py

--- cellarium/ml/models/nmf.py
+++ cellarium/ml/models/nmf.py
@@ -75,21 +75,59 @@ class NonNegativeMatrixFactorization(CellariumModel):
         """
         # def loss(a_nk: torch.Tensor) -> torch.Tensor:
         #     return torch.linalg.matrix_norm(x_ng - torch.matmul(a_nk, factors_kg), ord='fro')
-        
-        fit = torch.linalg.lstsq(factors_kg.T, x_ng.T)
-        alpha_nk = fit.solution.T
-
-        # an added non-negativity constraint, quite possibly wrong
-        alpha_nk = torch.clamp(alpha_nk, min=0.0)
-
-        residual_loss = fit.residuals.mean()
 
         n = x_ng.shape[0]  # division by n is shown in Mairal section 3.4.3
+        k = factors_kg.shape[0]
+
+        # def loss(a_nk: torch.Tensor) -> torch.Tensor:
+        #     return torch.linalg.matrix_norm(x_ng - torch.matmul(a_nk, factors_kg), ord='fro')
+
+        # fit = torch.linalg.lstsq(factors_kg.T, x_ng.T)
+        # alpha_nk = fit.solution.T
+
+        # an added non-negativity constraint, quite possibly wrong
+        # alpha_nk = torch.clamp(alpha_nk, min=0.0)
+
+        WWT_kk = torch.matmul(factors_kg, factors_kg.T)
+        WXT_kn = torch.matmul(factors_kg, x_ng.T)
+        alpha_nk = torch.zeros((n, k)).to(factors_kg.device)
+        alpha_nk = self.solve_alpha(alpha_nk, WWT_kk, WXT_kn, 1)
+
+        # residual_loss = fit.residuals.mean()
+
         self.A_kk = self.A_kk + torch.matmul(alpha_nk.T, alpha_nk) / n
         # TODO: see if this is faster than the matmul above: torch.einsum("ik,jk->ij", t, t)
         self.B_kg = self.B_kg + torch.matmul(alpha_nk.T, x_ng) / n
-        updated_factors_kg = self.dictionary_update(factors_kg)
-        return updated_factors_kg, residual_loss
+        updated_factors_kg = self.dictionary_update(factors_kg, 1)
+
+        return updated_factors_kg  # , residual_loss
+
+    def solve_alpha(self,
+                    alpha_nk: torch.Tensor,
+                    WWT_kk: torch.Tensor,
+                    WXT_kn: torch.Tensor,
+                    n_iterations: int) -> torch.Tensor:
+        """
+        Algorithm 2 from Mairal et al. [1] for computing the dictionary update.
+
+        Args:
+            factors_kg: The matrix of gene expression programs (Mairal's dictionary D).
+            n_iterations: The number of iterations to perform.
+        """
+
+        k_dimension = alpha_nk.shape[1]
+
+        for _ in range(n_iterations):
+            for k in range(k_dimension):
+                scalar = WWT_kk[k, k]
+                a_1k = WWT_kk[k, :]
+                b_1g = WXT_kn[k, :]
+
+                u_1g = alpha_nk[:, k] + (b_1g - torch.matmul(a_1k, alpha_nk.T)) / scalar
+
+                alpha_nk[:, k] = u_1g
+
+        return alpha_nk
     
     def dictionary_update(self, factors_kg: torch.Tensor, n_iterations: int = 1) -> torch.Tensor:
         """
@@ -134,9 +172,12 @@ class NonNegativeMatrixFactorization(CellariumModel):
         assert_columns_and_array_lengths_equal("x_ng", x_ng, "var_names_g", var_names_g)
         assert_arrays_equal("var_names_g", var_names_g, "var_names_g", self.var_names_g)
 
+        x_ = x_ng
+        x_ = torch.log1p(x_)
+
         if self.algorithm == "mairal":
-            self.D_kg, loss = self.online_dictionary_learning(x_ng=x_ng, factors_kg=self.D_kg)
-            print('loss', loss)
+            self.D_kg = self.online_dictionary_learning(x_ng=x_, factors_kg=self.D_kg)
+            # print('loss', loss)
             assert (self.D_kg < 0.0).sum() == 0, 'there are negative elements in the dictionary matrix'
         else:
             raise ValueError(f"Unknown algorithm: {self.algorithm}")
@@ -157,10 +198,23 @@ class NonNegativeMatrixFactorization(CellariumModel):
         Inferred gene expression programs (i.e. "factors").
         """
         return self.D_kg
-    
-    def predict(self, x_ng: torch.Tensor) -> torch.Tensor:
+
+    def predict(self, x_ng: torch.Tensor, var_names_g: np.ndarray) -> dict[str, np.ndarray | torch.Tensor]:
         """
         Predict the gene expression programs for the given gene counts matrix.
         """
-        # something using the ANLS solver ...
-        raise NotImplementedError
+        assert_columns_and_array_lengths_equal("x_ng", x_ng, "var_names_g", var_names_g)
+        assert_arrays_equal("var_names_g", var_names_g, "var_names_g", self.var_names_g)
+
+        n = x_ng.shape[0]
+        k = self.D_kg.shape[0]
+
+        x_ = x_ng
+        x_ = torch.log1p(x_)
+
+        WWT_kk = torch.matmul(self.D_kg, self.D_kg.T)
+        WXT_kn = torch.matmul(self.D_kg, x_.T)
+        alpha_nk = torch.zeros((n, k)).to(self.D_kg.device)
+        alpha_nk = self.solve_alpha(alpha_nk, WWT_kk, WXT_kn, 1000)
+
+        return alpha_nk
