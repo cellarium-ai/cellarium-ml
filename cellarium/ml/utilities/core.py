a38b3cf1bd564275fc029092dfbd778f23b52bb7
Yerdos Ordabayev <yordabay@broadinstitute.org>
2024-07-24 16:24:15 -0400

    Remove reduntant resolve_ckpt method (#214)
    * remove reduntant resolve_ckpt method
    
    * add resolve_ckpt_dir to utilities

--- cellarium/ml/utilities/core.py
+++ cellarium/ml/utilities/core.py
@@ -3,9 +3,12 @@
 
 import copy
 import math
+import os
 from collections.abc import Callable
+from pathlib import Path
 from typing import Any
 
+import lightning.pytorch as pl
 import torch
 
 from cellarium.ml.utilities.testing import assert_nonnegative, assert_positive
@@ -134,3 +137,22 @@ def call_func_with_batch(
     if "kwargs" in ann:
         input_keys |= batch.keys()
     return func(**{key: batch[key] for key in input_keys})
+
+
+def resolve_ckpt_dir(trainer: pl.Trainer) -> Path | str:
+    if len(trainer.loggers) > 0:
+        if trainer.loggers[0].save_dir is not None:
+            save_dir = trainer.loggers[0].save_dir
+        else:
+            save_dir = trainer.default_root_dir
+        name = trainer.loggers[0].name
+        version = trainer.loggers[0].version
+        version = version if isinstance(version, str) else f"version_{version}"
+        ckpt_path = os.path.join(save_dir, str(name), version, "checkpoints")
+    else:
+        # if no loggers, use default_root_dir
+        ckpt_path = os.path.join(trainer.default_root_dir, "checkpoints")
+
+    os.makedirs(ckpt_path, exist_ok=True)
+
+    return ckpt_path
