f50462c2a1371e3f9ee20620b8564a1f9a26bc56
Yang Xu <yxu9211@gmail.com>
2024-07-30 15:14:57 -0400

    Update nmf.py

--- cellarium/ml/models/nmf.py
+++ cellarium/ml/models/nmf.py
@@ -25,7 +25,7 @@ import pandas as pd
 # from sklearn.cluster import KMeans
 
 class KMeans:
-    def __init__(self, n_clusters, max_iter=300, tol=1e-4):
+    def __init__(self, n_clusters, max_iter=200, tol=1e-4):
         self.n_clusters = n_clusters
         self.max_iter = max_iter
         self.tol = tol
