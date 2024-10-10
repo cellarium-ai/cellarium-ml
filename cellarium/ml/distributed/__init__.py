9ea4f1ac45b60b6b6e4cd002dfb658056ed48281
Yerdos Ordabayev <yordabay@broadinstitute.org>
2024-10-10 14:56:40 -0400

    remove obsolte code (#252)

--- cellarium/ml/distributed/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright Contributors to the Cellarium project.
-# SPDX-License-Identifier: BSD-3-Clause
-
-from cellarium.ml.distributed.gather import GatherLayer
-
-__all__ = [
-    "GatherLayer",
-]
