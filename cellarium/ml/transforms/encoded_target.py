3de79cb5f23cc613999d574df9da583a33bcce24
nimish <nmagre@broadinstitute.org>
2024-10-11 17:54:10 +0000

    modified encoded targets

--- cellarium/ml/transforms/encoded_target.py
+++ cellarium/ml/transforms/encoded_target.py
@@ -10,18 +10,18 @@ from cellarium.ml.data.fileio import read_pkl_from_gcs
 
 class EncodedTargets(nn.Module):
     """
-when called, assigns multilabel targets. All parents of the target cell type get assigned as targets. 
+    when called, assigns multilabel targets. All parents of the target cell type get assigned as targets. 
     """
 
     def __init__(
         self,
-        multilabel_flag: bool = True,
-        child_parent_list_path: str = '/home/nmagre/P1_logistic_regression/child_parent_indices_list.pkl',
+        multilabel_flag: bool = False,
+        target_ancestors_list_path: str = 'gs://cellarium-file-system/curriculum/human_10x_ebd_lrexp_extract/models/shared_metadata/target_ancestors_list.pkl',
         unique_cell_types_nparray_path: str = 'gs://cellarium-file-system/curriculum/human_10x_ebd_lrexp_extract/models/shared_metadata/final_filtered_sorted_unique_cells.pkl',
     ) -> None:
         super().__init__()
         self.multilabel_flag = multilabel_flag
-        self.child_parent_list = read_pkl_from_gcs(child_parent_list_path)
+        self.target_ancestors_list = read_pkl_from_gcs(target_ancestors_list_path)
         self.unique_cell_types_nparray = read_pkl_from_gcs(unique_cell_types_nparray_path)
 
 
@@ -34,9 +34,9 @@ when called, assigns multilabel targets. All parents of the target cell type get
         if self.multilabel_flag==0:
             return({'y_n':torch.tensor(np.searchsorted(self.unique_cell_types_nparray, y_n))})
         else:
-            out_array = np.zeros((len(y_n), len(self.child_parent_list)), dtype=int)
+            out_array = np.zeros((len(y_n), len(self.target_ancestors_list)), dtype=int)
             indices = np.searchsorted(self.unique_cell_types_nparray, y_n)
             for i, target_index in enumerate(indices):
                 # Set the corresponding columns to 1
-                out_array[i, [target_index]+self.child_parent_list[target_index]] = 1
+                out_array[i, [target_index]+self.target_ancestors_list[target_index]] = 1
             return {'y_n':torch.tensor(out_array)}
