43893b4c75047dea328caa18972f507a723daef8
Yerdos Ordabayev <ordabayev@cer-login-03.ai.alcf.anl.gov>
2024-11-01 14:40:15 +0000

    import from layers

--- cellarium/ml/utilities/mup.py
+++ cellarium/ml/utilities/mup.py
@@ -6,8 +6,45 @@ from __future__ import annotations
 import fnmatch
 import re
 from collections.abc import Callable
+from typing import Any
 
 from torch import nn
+
+
+def scale_initializers_by_dimension(
+    initializers: dict[str, Any] | list[dict[str, Any]],
+    width_scale: float | None = None,
+    depth_scale: float | None = None,
+) -> None:
+    """
+    Scales the std of an initializer or list of initializers by the specified
+    width and depth scalars. Unsupported initializers are ignored and a warning
+    is printed to the user.
+    """
+    if not width_scale:
+        width_scale = 1.0
+    if not depth_scale:
+        depth_scale = 1.0
+    mup_scalar = width_scale * depth_scale
+
+    if not isinstance(initializers, list):
+        initializers = [initializers]
+
+    for initializer in initializers:
+        if "name" not in initializer:
+            raise ValueError("Initializer name must be provided")
+        initializer_name = initializer["name"].lower()
+
+        if initializer_name == "normal_":
+            initializer["std"] = initializer.get("std", 1.0) * mup_scalar
+        elif initializer_name == "trunc_normal_":
+            std = initializer.get("std", 1.0)
+            initializer["std"] = std * mup_scalar
+            initializer["a"] = initializer.get("a", -2 * std) * mup_scalar
+            initializer["b"] = initializer.get("b", 2 * std) * mup_scalar
+            std = None
+        else:
+            raise ValueError(f"Initializer {initializer_name} is not supported for muP")
 
 
 def convert_glob_to_regex(f: str) -> re.Pattern:
