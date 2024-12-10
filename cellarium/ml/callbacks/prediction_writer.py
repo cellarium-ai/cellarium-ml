15908f8009de5d97f7c581903173ebbc9f9de705
nimish <nmagre@broadinstitute.org>
2024-12-10 20:32:14 +0000

    trial 2

--- cellarium/ml/callbacks/prediction_writer.py
+++ cellarium/ml/callbacks/prediction_writer.py
@@ -21,6 +21,7 @@ def write_prediction(
     query_ids: np.ndarray,
     output_dir: Path | str,
     columns: np.ndarray,
+    postfix: int | str
 ) -> None:
     """
     Write prediction to a CSV file.
@@ -41,7 +42,7 @@ def write_prediction(
     if not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
 
-    output_path = "gs://cellarium-file-system/curriculum/lrexp_human_validation_split_20241126/model_predictions/Base_model_regular_lr_no_pp/hop_scores_extract_1_110.csv"
+    output_path = "gs://cellarium-file-system/curriculum/lrexp_human_validation_split_20241126/model_predictions/Base_model_regular_lr_no_pp/hop_scores_extract_"+str(postfix)+"_lp_logits_only.csv"
     cas_out_csv.sort_values(by='query_cell_id', inplace=True)
     cas_out_csv.to_csv(output_path, header=True, index=False)
 
@@ -94,4 +95,5 @@ class PredictionWriter(pl.callbacks.BasePredictionWriter):
             query_ids = y_n_cell_type_ids,
             output_dir=self.output_dir,
             columns=columns[0:self.prediction_size],
+            postfix=batch_idx * trainer.world_size + trainer.global_rank
         )
