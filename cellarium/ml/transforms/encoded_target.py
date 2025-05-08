3c98fc287da1d1ccdaaa137c2454fe3ac24d6a9a
nimish <nmagre@broadinstitute.org>
2025-05-08 15:22:28 +0000

    SOCAM TRIAL 1 FOR ONLY SINGLE TARGET MODELS

--- cellarium/ml/transforms/encoded_target.py
+++ cellarium/ml/transforms/encoded_target.py
@@ -15,19 +15,16 @@ class EncodedTargets(nn.Module):
 
     def __init__(
         self,
-        multilabel_flag: bool = False,
-        target_row_ancestors_col_torch_tensor_path: str = '',
+        # target_row_ancestors_col_torch_tensor_path: str = '',
         unique_cell_types_nparray_path: str = '',
-        target_row_descendents_only_col_torch_tensor_path: str = '',
-        target_row_mod_col_torch_tensor_path: str = ''
+        # target_row_descendents_only_col_torch_tensor_path: str = '',
+        # target_row_mod_col_torch_tensor_path: str = ''
     ) -> None:
         super().__init__()
-        self.multilabel_flag = multilabel_flag
-        self.target_row_ancestors_col_torch_tensor = read_pkl_from_gcs(target_row_ancestors_col_torch_tensor_path)
+        # self.target_row_ancestors_col_torch_tensor = read_pkl_from_gcs(target_row_ancestors_col_torch_tensor_path)
         self.unique_cell_types_nparray = read_pkl_from_gcs(unique_cell_types_nparray_path)
-        self.target_row_descendents_only_col_torch_tensor = read_pkl_from_gcs(target_row_descendents_only_col_torch_tensor_path)
-        self.target_row_mod_col_torch_tensor = read_pkl_from_gcs(target_row_mod_col_torch_tensor_path)
-
+        # self.target_row_descendents_only_col_torch_tensor = read_pkl_from_gcs(target_row_descendents_only_col_torch_tensor_path)
+        # self.target_row_mod_col_torch_tensor = read_pkl_from_gcs(target_row_mod_col_torch_tensor_path)
 
     def forward(
         self,y_n: np.ndarray
@@ -36,7 +33,4 @@ class EncodedTargets(nn.Module):
 
         """
         indices = np.searchsorted(self.unique_cell_types_nparray, y_n)
-        if self.multilabel_flag==0:
-            return {'y_n':torch.tensor(indices), 'descendents_nc': self.target_row_descendents_only_col_torch_tensor[indices], 'mod_nc': self.target_row_mod_col_torch_tensor[indices], 'y_n_predict': indices} # using for prediction
-        else:
-            return {'y_n':self.target_row_ancestors_col_torch_tensor[indices], 'descendents_nc': self.target_row_descendents_only_col_torch_tensor[indices], 'mod_nc': self.target_row_mod_col_torch_tensor[indices], 'y_n_predict': indices} # using for prediction
+        return {'y_n':torch.tensor(indices), 'y_n_predict': indices} # using for prediction
