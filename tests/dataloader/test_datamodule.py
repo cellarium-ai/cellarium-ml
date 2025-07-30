ca09d140387217b23e0a81a7d4a32297368379ea
Stephen Fleming <sjfleming@users.noreply.github.com>
2025-07-30 14:04:15 -0400

    allow backed mode CellariumAnnDataDataModule load to pass

--- tests/dataloader/test_datamodule.py
+++ tests/dataloader/test_datamodule.py
@@ -178,11 +178,12 @@ def test_cpu_transforms(
 )
 @pytest.mark.parametrize("batch_size", [50, None])
 def test_datamodule(tmp_path: Path, batch_size: int | None, accelerator: str) -> None:
+    dadc = DistributedAnnDataCollection(
+        filenames="https://storage.googleapis.com/dsp-cellarium-cas-public/test-data/test_0.h5ad",
+        shard_size=100,
+    )
     datamodule = CellariumAnnDataDataModule(
-        DistributedAnnDataCollection(
-            filenames="https://storage.googleapis.com/dsp-cellarium-cas-public/test-data/test_0.h5ad",
-            shard_size=100,
-        ),
+        dadc=dadc,
         batch_size=100,
         batch_keys={
             "x_ng": AnnDataField(attr="X", convert_fn=densify),
@@ -193,15 +194,14 @@ def test_datamodule(tmp_path: Path, batch_size: int | None, accelerator: str) ->
     trainer.fit(module, datamodule)
 
     ckpt_path = str(tmp_path / "lightning_logs/version_0/checkpoints/epoch=0-step=1.ckpt")
-    adata = datamodule.dadc.adatas[0].adata
-    kwargs = {"dadc": adata}
+    kwargs = {"dadc": dadc}
     if batch_size is not None:
         kwargs["batch_size"] = batch_size
     loaded_datamodule = CellariumAnnDataDataModule.load_from_checkpoint(ckpt_path, **kwargs)
 
     assert loaded_datamodule.batch_keys == datamodule.batch_keys
     assert loaded_datamodule.batch_size == batch_size or datamodule.batch_size
-    assert loaded_datamodule.dadc is adata
+    assert loaded_datamodule.dadc is dadc
 
 
 @pytest.mark.parametrize(
