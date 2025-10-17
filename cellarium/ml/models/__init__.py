4ade47607379e368cbb5fef0fbcd08fa256d9589
Stephen Fleming <sjfleming@users.noreply.github.com>
2025-10-17 02:25:52 -0400

    renaming files

--- cellarium/ml/models/__init__.py
+++ cellarium/ml/models/__init__.py
@@ -7,11 +7,13 @@ from cellarium.ml.models.incremental_pca import IncrementalPCA
 from cellarium.ml.models.logistic_regression import LogisticRegression
 from cellarium.ml.models.model import CellariumModel, PredictMixin, TestMixin, ValidateMixin
 from cellarium.ml.models.nmf import OnlineNonNegativeMatrixFactorization
+from cellarium.ml.models.nmf_bayesian import BayesianNonNegativeMatrixFactorization
 from cellarium.ml.models.onepass_mean_var_std import OnePassMeanVarStd
 from cellarium.ml.models.probabilistic_pca import ProbabilisticPCA
 from cellarium.ml.models.tdigest import TDigest
 
 __all__ = [
+    "BayesianNonNegativeMatrixFactorization",
     "CellariumGPT",
     "CellariumModel",
     "Geneformer",
