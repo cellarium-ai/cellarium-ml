a382e2281d2ed8207dbf1f3cb7494a97a0d7dc77
Stephen Fleming <sjfleming@users.noreply.github.com>
2026-05-21 17:00:15 -0400

    remove pyro distribution

--- cellarium/ml/distributions/__init__.py
+++ cellarium/ml/distributions/__init__.py
@@ -2,6 +2,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from cellarium.ml.distributions.negative_binomial import NegativeBinomial
-from cellarium.ml.distributions.unconstrained_categorical import UnconstrainedPyroCategorical
 
-__all__ = ["NegativeBinomial", "UnconstrainedPyroCategorical"]
+__all__ = ["NegativeBinomial"]
