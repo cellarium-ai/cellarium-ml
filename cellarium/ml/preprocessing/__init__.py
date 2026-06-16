1a639ef4e6fe54d25533fd3af598c01b2aa1bb23
Stephen Fleming <sjfleming@users.noreply.github.com>
2026-06-16 17:28:22 -0400

    import kotliar at module level, use in tests

--- cellarium/ml/preprocessing/__init__.py
+++ cellarium/ml/preprocessing/__init__.py
@@ -1,6 +1,12 @@
 # Copyright Contributors to the Cellarium project.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from cellarium.ml.preprocessing.highly_variable_genes import seurat_compute_highly_variable_genes
+from cellarium.ml.preprocessing.highly_variable_genes import (
+    kotliar_compute_highly_variable_genes,
+    seurat_compute_highly_variable_genes,
+)
 
-__all__ = ["seurat_compute_highly_variable_genes"]
+__all__ = [
+    "kotliar_compute_highly_variable_genes",
+    "seurat_compute_highly_variable_genes",
+]
